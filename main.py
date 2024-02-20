693e9e2e35f301e5618868a91ee3aaf91181a8a8
NIKOLAI AZAROV <azarovnic@yandex.ru>
2024-02-20 08:24:41 +0300

    Реализована функция "word_multiplier"

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-print('My first file')
-print('NewLine WOW!')
+def word_multiplier(word: str, num: int) -> str:
+    word = word.capitalize()
+    return word * num
+
+word = input('Введите слово: ')
+num = int(input('Введите количество повторений: '))
+
+print(word_multiplier(word, num))
